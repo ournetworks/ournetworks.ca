75ffff2af2e1bc774b792ca1a332148a383e9f99
Garry Ing <garry.ing@gmail.com>
2018-02-25 16:06:03 -0500

    confirmation bar

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+  if (window.location.search == '?signup=confirmed') {
+    var post = document.querySelector('.header');
+
+    var confirmationEle = document.createElement('div');
+    confirmationEle.className = 'announcement';
+    confirmationEle.setAttribute('role', 'alert');
+    confirmationEle.innerHTML = '<div class="container"><p>Email submitted successfully</p></div>';
+
+    post.append(confirmationEle);
+    document.querySelector('.post').classList.add('extended');
+  }
+});
