bc0c3143a5d4873d27b1f6404da9210469823fad
Garry Ing <garry.ing@gmail.com>
2018-02-25 16:15:44 -0500

    copy change

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function(){
     var confirmationEle = document.createElement('div');
     confirmationEle.className = 'announcement';
     confirmationEle.setAttribute('role', 'alert');
-    confirmationEle.innerHTML = '<div class="container"><p>Email submitted successfully</p></div>';
+    confirmationEle.innerHTML = '<div class="container"><p>Successfully signed up for updates</p></div>';
 
     post.append(confirmationEle);
     document.querySelector('.splash').classList.add('extended');
