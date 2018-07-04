a8b986c4f5716bb1270a5fe0e2aeee47479d47f1
Garry Ing <garry.ing@gmail.com>
2018-07-04 10:31:07 -0400

    intial embed front end

---
+++ js/live-stream-embed.js
@@ -0,0 +1,67 @@
+// Live Stream Video
+
+var ipfs_gateway_self = 'http://mesh.world:8080';  // IPFS gateway of this node
+var ipfs_gateway_origin = 'http://mesh.world:8080';  // IPFS gateway of origin stream
+var m3u8_ipfs = 'http://mesh.world/live.m3u8';  // File path to m3u8 with IPFS content via HTTP server
+// var m3u8_ipfs='__IPFS_GATEWAY_ORIGIN__/ipns/__IPFS_ID_ORIGIN__';  // URL to m3u8 via IPNS (uncomment to enable)
+var m3u8_http_urls = [];  // Optional list of URLs to m3u8 over HTTP
+
+var live = videojs('live');
+
+function httpStream() {
+  live.src({
+    src: m3u8_http_urls[Math.floor(Math.random() * m3u8_http_urls.length)],
+    type: 'application/x-mpegURL',
+  });
+  loadStream();
+}
+
+function ipfsStream() {
+  live.src({
+    src: m3u8_ipfs,
+    type: 'application/x-mpegURL',
+  });
+  loadStream();
+  videojs.Hls.xhr.beforeRequest = function(options) {
+    // Replace IPFS gateway of origin with that of this node
+    options.uri = options.uri.replace(ipfs_gateway_origin, ipfs_gateway_self);
+    if (options.uri.indexOf('/ipfs/')) {
+      document.getElementById('loadingTitle').innerHTML='Located stream via IPNS'
+      document.getElementById('msg').innerHTML='Downloading video content...'
+    }
+    console.debug(options.uri);
+    return options;
+  };
+}
+
+function loadStream() {
+  document.getElementById('LoadingStream').style.display = 'block';
+  document.getElementById('SelectStream').style.display='none';
+}
+
+document.querySelector('.stream-option').addEventListener('click', function(event){
+  if (event.currentTarget.classList.contains('ipfs-stream')) {
+    ipfsStream();
+  } else {
+    httpStream();
+  }
+});
+
+live.metadata='none';
+
+live.on('loadedmetadata', function() {
+  document.getElementById('StreamSelecter').style.display = 'none';
+});
+
+live.on('loadeddata', function(event) {
+  console.debug(event);
+});
+
+live.on('error', function(event) {
+  console.debug(this.error());
+  document.getElementById('msg').innerHTML=this.error().message;
+});
+
+if (!m3u8_http_urls || !Array.isArray(m3u8_http_urls) || (m3u8_http_urls.length==0)) {
+  document.getElementById('clearStream').style.display='none';
+}
