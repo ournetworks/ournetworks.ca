fdcbe89a06ea1d0fa4d4406f087410316e839a1f
Garry Ing <garry.ing@gmail.com>
2018-07-08 19:47:00 -0400

    formatting fixes

--- js/live-stream-embed.js
+++ js/live-stream-embed.js
@@ -1,15 +1,15 @@
 // Live Stream Video
 
-var ipfs_gateway_self = 'http://ipfs-mirror-0.mesh.world:8080';  // IPFS gateway of this node
-var ipfs_gateway_origin = 'http://ipfs-mirror-0.mesh.world:8080';  // IPFS gateway of origin stream
-var m3u8_ipfs = 'http://ipfs-mirror-0.mesh.world/live.m3u8';  // File path to m3u8 with IPFS content via HTTP server
-// var m3u8_ipfs='__IPFS_GATEWAY_ORIGIN__/ipns/__IPFS_ID_ORIGIN__';  // URL to m3u8 via IPNS (uncomment to enable)
-var m3u8_http_urls = ['http://ipfs-mirror-0.mesh.world/live.m3u8'];  // Optional list of URLs to m3u8 over HTTP
+var ipfs_gateway_self = 'http://ipfs-mirror-0.mesh.world:8080'; // IPFS gateway of this node
+var ipfs_gateway_origin = 'http://ipfs-mirror-0.mesh.world:8080'; // IPFS gateway of origin stream
+var m3u8_ipfs = 'http://ipfs-mirror-0.mesh.world/live.m3u8'; // File path to m3u8 with IPFS content via HTTP server
+// var m3u8_ipfs='__IPFS_GATEWAY_ORIGIN__/ipns/__IPFS_ID_ORIGIN__'; // URL to m3u8 via IPNS (uncomment to enable)
+var m3u8_http_urls = ['http://ipfs-mirror-0.mesh.world/live.m3u8']; // Optional list of URLs to m3u8 over HTTP
 
 var live = videojs('live');
 
 // For any browser except Safari
-if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent) == false) {
+if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent) === false) {
   // Override native player for platform and browser consistency
   videojs.options.html5.nativeAudioTracks = false;
   videojs.options.html5.nativeVideoTracks = false;
@@ -19,7 +19,7 @@ if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent) == false) {
 function httpStream() {
   live.src({
     src: m3u8_http_urls[Math.floor(Math.random() * m3u8_http_urls.length)],
-    type: 'application/x-mpegURL',
+    type: 'application/x-mpegURL'
   });
   loadStream();
 }
@@ -27,15 +27,15 @@ function httpStream() {
 function ipfsStream() {
   live.src({
     src: m3u8_ipfs,
-    type: 'application/x-mpegURL',
+    type: 'application/x-mpegURL'
   });
   loadStream();
   videojs.Hls.xhr.beforeRequest = function(options) {
     // Replace IPFS gateway of origin with that of this node
     options.uri = options.uri.replace(ipfs_gateway_origin, ipfs_gateway_self);
     if (options.uri.indexOf('/ipfs/')) {
-      document.getElementById('loadingTitle').innerHTML = 'Located stream via IPFS'
-      document.getElementById('msg').innerHTML = 'Downloading video content...'
+      document.getElementById('loadingTitle').innerHTML = 'Located stream via IPFS';
+      document.getElementById('msg').innerHTML = 'Downloading video content...';
     }
     console.debug(options.uri);
     return options;
@@ -47,15 +47,15 @@ function loadStream() {
   document.getElementById('SelectStream').style.display = 'none';
 }
 
-document.querySelector('.ipfs-stream').addEventListener('click', function(event){
+document.querySelector('.ipfs-stream').addEventListener('click', function(event) {
   ipfsStream();
 });
 
-document.querySelector('.http-stream').addEventListener('click', function(event){
+document.querySelector('.http-stream').addEventListener('click', function(event) {
   httpStream();
 });
 
-live.metadata='none';
+live.metadata = 'none';
 
 live.on('loadedmetadata', function() {
   document.getElementById('StreamSelector').style.display = 'none';
@@ -67,11 +67,11 @@ live.on('loadeddata', function(event) {
 
 live.on('error', function(event) {
   console.debug(this.error());
-  document.getElementById('loadingTitle').innerHTML = 'Unable to load live stream'
+  document.getElementById('loadingTitle').innerHTML = 'Unable to load live stream';
   document.querySelector('.loader-animation').style.display = 'none';
   document.getElementById('msg').innerHTML = this.error().message;
 });
 
-if (!m3u8_http_urls || !Array.isArray(m3u8_http_urls) || (m3u8_http_urls.length == 0)) {
+if (!m3u8_http_urls || !Array.isArray(m3u8_http_urls) || (m3u8_http_urls.length === 0)) {
   document.querySelector('.http-stream').setAttribute('disabled', 'disabled');
 }
