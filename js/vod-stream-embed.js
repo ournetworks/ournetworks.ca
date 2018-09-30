14df0eb5cead48e1d8e010f2a8b3a49f35e4da26
Benedict Lau <ben.hy.lau@gmail.com>
2018-09-30 23:22:26 +0200

    Add video-on-demand player for recorded talks (#79)
    * Add video-on-demand player for recorded talks
    
    * Add standalone video-on-demand player with ipfs+http dynamic stream support
    
    * Add the official video-on-demand player page
    
    * Clean up video-on-demand player
    
    * syntax stuff
    
    * page rename and error condition

---
+++ js/vod-stream-embed.js
@@ -0,0 +1,23 @@
+// Video-on-demand Stream Video
+
+var ipfs_gateway_self = 'https://ipfs.io'; // IPFS gateway of this node
+var ipfs_gateway_origin = 'https://ipfs.io'; // IPFS gateway of origin stream
+
+var stream_type = 'video/mp4'; // Type of video stream
+var stream_url_ipfs = 'https://ipfs.io/ipfs/'; // File path to stream with IPFS content via HTTP server
+var stream_urls_http = []; // Optional list of URLs to stream over HTTP
+
+// Replace IPFS gateway of origin with that of this node
+stream_url_ipfs = stream_url_ipfs.replace(ipfs_gateway_origin, ipfs_gateway_self);
+
+// Append IPFS content to stream URL
+var ipfs_param = new URLSearchParams(window.location.search).get('ipfs');
+if (ipfs_param) {
+  stream_url_ipfs = stream_url_ipfs + ipfs_param;
+}
+
+// Add URL to stream over HTTP if available
+var http_param = new URLSearchParams(window.location.search).get('http');
+if (http_param) {
+  stream_urls_http.push(http_param);
+}
---
+++ video.md
@@ -0,0 +1,8 @@
+---
+layout: page
+order: 0
+title: "Video Player"
+titleDisplay: "Video Player"
+---
+
+{% include vod-stream-embed.html %}
