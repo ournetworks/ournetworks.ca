26aaa6fc25cd66fecac388a75d69abc7265347dd
dcwalk <dcwalk@users.noreply.github.com>
2018-10-02 10:06:15 -0400

    Add Recorded Talks page (#78)
    * remove unused start/end times fields
    * initial structure for recorded talks
    * update talk announcement
    * add youtube links to session data
    * remove front part of youtube link
    * update to display of recorded talks
    * fix presenter link
    * orga condition
    * update thanks order
    * add ipfs 720 1080 links and update names
    * update recorded talks page
    * add alt take to thumbnail images
    * Add video-on-demand player for recorded talks (#79)
    * Add video-on-demand player for recorded talks
    * Add standalone video-on-demand player with ipfs+http dynamic stream support
    * Add the official video-on-demand player page
    * Clean up video-on-demand player
    * syntax stuff
    * page rename and error condition
    * updates
    * minor updates revert to external links
    * add ipfs icon
    * fix ipfx icon
    * play overlay
    * flip
    * border video links
    * svg clean up
    * update video size indication
    * Change player gateway to ipfs.infura.io

---
+++ js/video-player-embed.js
@@ -0,0 +1,81 @@
+var live = videojs('live');
+
+// For any browser except Safari
+if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent) === false) {
+  // Override native player for platform and browser consistency
+  videojs.options.html5.nativeAudioTracks = false;
+  videojs.options.html5.nativeVideoTracks = false;
+  videojs.options.hls.overrideNative = true;
+}
+
+function httpStream() {
+  live.src({
+    src: stream_urls_http[Math.floor(Math.random() * stream_urls_http.length)],
+    type: stream_type
+  });
+  loadStream();
+}
+
+function ipfsStream() {
+  live.src({
+    src: stream_url_ipfs,
+    type: stream_type
+  });
+  loadStream();
+  videojs.Hls.xhr.beforeRequest = function(options) {
+    // Replace IPFS gateway of origin with that of this node
+    options.uri = options.uri.replace(ipfs_gateway_origin, ipfs_gateway_self);
+    if (options.uri.indexOf('/ipfs/')) {
+      document.getElementById('loadingTitle').innerHTML = 'Located stream via IPFS';
+      document.getElementById('msg').innerHTML = 'Downloading video content...';
+    }
+    console.debug(options.uri);
+    return options;
+  };
+}
+
+function loadStream() {
+  document.getElementById('LoadingStream').style.display = 'block';
+  document.getElementById('SelectStream').style.display = 'none';
+}
+
+document.querySelector('.ipfs-stream').addEventListener('click', function(event) {
+  ipfsStream();
+});
+
+document.querySelector('.http-stream').addEventListener('click', function(event) {
+  httpStream();
+});
+
+live.metadata = 'none';
+
+live.on('loadedmetadata', function() {
+  document.getElementById('StreamSelector').style.display = 'none';
+});
+
+live.on('loadeddata', function(event) {
+  console.debug(event);
+});
+
+var refreshButton = document.createElement('button');
+refreshButton.className = 'button button-primary compact stream-refresh';
+refreshButton.innerHTML = 'Refresh Page and Try Again';
+refreshButton.addEventListener('click', function() {
+  window.location.reload(true);
+});
+
+live.on('error', function(event) {
+  console.debug(this.error());
+  if (window.location.href.indexOf('livestream') > -1) {
+    document.getElementById('loadingTitle').innerHTML = 'Unable to load live stream';
+  } else {
+    document.getElementById('loadingTitle').innerHTML = 'Unable to load video';
+  }
+  document.querySelector('.loader-animation').style.display = 'none';
+  document.getElementById('msg').innerHTML = this.error().message;
+  document.getElementById('LoadingStream').appendChild(refreshButton);
+});
+
+if (!stream_urls_http || !Array.isArray(stream_urls_http) || (stream_urls_http.length === 0)) {
+  document.querySelector('.http-stream').setAttribute('disabled', 'disabled');
+}
---
+++ streamz.md
@@ -0,0 +1,65 @@
+---
+layout: page
+order: 0
+title: "Stream"
+titleDisplay: "Stream"
+---
+
+<style type="text/css">
+  #globe {
+    position: fixed;
+    z-index: 100
+  }
+</style>
+
+<script type="text/javascript">
+  let topPos = 100;
+  let lastTopPos = 95;
+  let leftPos = 100;
+  let lastLeftPos = 95;
+
+  const animateTop = () => {
+    if (topPos > window.innerHeight || topPos < 0) {
+      const temp = lastTopPos;
+      lastTopPos = topPos;
+      topPos = temp;
+    } else if (lastTopPos > topPos) {
+      lastTopPos = topPos;
+      topPos -= 5;
+    } else {
+      lastTopPos = topPos;
+      topPos += 5;
+    }
+  }
+
+  const animateLeft = () => {
+    if (leftPos > window.innerWidth - 50 || leftPos < -50) {
+      const temp = lastLeftPos;
+      lastLeftPos = leftPos;
+      leftPos = temp;
+    } else if (lastLeftPos > leftPos) {
+      lastLeftPos = leftPos
+      leftPos -= 5;
+    } else {
+      lastLeftPos = leftPos
+      leftPos += 5;
+    }
+  }
+
+  const bounce = () => {
+    setTimeout(() => {
+      const globe = document.getElementById("globe");
+      globe.style.top = `${topPos}px`;
+      globe.style.left = `${leftPos}px`;
+      animateTop();
+      animateLeft();
+      bounce();
+    }, 10);
+  }
+
+  bounce();
+</script>
+
+<img src="/images/yami-ichi/globe.gif" alt="globe" id="globe" />
+
+{% include vod-stream-embed.html %}
