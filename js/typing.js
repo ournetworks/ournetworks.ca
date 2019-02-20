5cfe53596f7d52833b4bc902fa26b6843b5a943b
dcwalk <dc@dcwalker.ca>
2019-02-19 19:27:04 -0500

    move the js word animation thing around

---
+++ js/typing.js
@@ -0,0 +1,39 @@
+const words = ['Regeneration', 'Renewal', 'Remediation', 'Repair', 'Refusal', 'Relation', 'Reproduction', 'Reenchantment', 'Resistance']
+let i = 0
+let timer
+
+function typingEffect () {
+  let word = words[i].split("")
+  var loopTyping = function () {
+    if (word.length > 0) {
+      document.getElementById('word').innerHTML += word.shift()
+    } else {
+      deletingEffect()
+      return false
+    };
+    timer = setTimeout(loopTyping, 200)
+  }
+  loopTyping()
+};
+
+function deletingEffect () {
+  let word = words[i].split("")
+  var loopDeleting = function () {
+    if (word.length > 0) {
+      word.pop()
+      document.getElementById('word').innerHTML = word.join("")
+    } else {
+      if (words.length > (i + 1)) {
+        i++
+      } else {
+        i = 0
+      };
+      typingEffect()
+      return false
+    };
+    timer = setTimeout(loopDeleting, 150)
+  }
+  loopDeleting()
+};
+
+typingEffect()
