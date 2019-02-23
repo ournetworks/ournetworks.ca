75f68260f2b5ad3ca5cd6f943fc93c375d85f96a
Garry Ing <garry.ing@gmail.com>
2019-02-23 15:26:33 -0500

    revised typing

--- js/typing.js
+++ js/typing.js
@@ -8,7 +8,7 @@ function typingEffect () {
     if (word.length > 0) {
       document.getElementById('word').innerHTML += word.shift()
     } else {
-      deletingEffect()
+      setTimeout(deletingEffect, 4000)
       return false
     };
     timer = setTimeout(loopTyping, 200)
@@ -28,12 +28,29 @@ function deletingEffect () {
       } else {
         i = 0
       };
-      typingEffect()
+      setTimeout(typingEffect, 1000)
       return false
     };
     timer = setTimeout(loopDeleting, 150)
   }
   loopDeleting()
 };
+
+function blockTyping () {
+  const paragraphBlock = document.getElementsByClassName('typing')
+  const paragraphBlockEl = [...paragraphBlock[0].children]
+  paragraphBlockEl.map(function(i, int){
+    setTimeout(function(){
+      i.classList.add('visible')
+      if (window.matchMedia('(min-width: 736px)').matches) {
+         paragraphBlock[0].scrollTop = i.offsetTop
+      }
+    }, 3100 * int)
+  });
+}
+
+
+  blockTyping()
+
 
 typingEffect()
