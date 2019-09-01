fbfddb410b4fb304e51cb319886683a9d5a90587
Benedict Lau <b@deprecated.systems>
2019-08-31 20:25:57 -0400

    Add CI script to add TXT record to DigitalOcean

---
+++ scripts/txt.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+#
+# Update TXT record for a domain managed by Digital Ocean.
+#
+# Usage:
+#
+#     export DO_TOKEN=<do_token>
+#     ./txt.sh <domain> <name> <value>
+#
+# Example:
+#
+#     ./txt.sh ournetworks.ca ipfs-website "ipfs/QmQQfD1ztg1aG82VmUmutusbxn7tL4c2o1qHv1ivyWbNj6"
+#
+#
+DOMAIN=$1
+NAME=$2
+VALUE=$3
+
+# List existing records with $NAME under $DOMAIN
+RESP_LIST=$(curl -sS -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DO_TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records")
+
+# Delete existing records with $NAME
+DELETES=$(echo $RESP_LIST | jq -c '.domain_records[] | select(.type == "TXT" and .name == "'"$NAME"'") | .id')
+for id in $DELETES; do
+  curl -sS -X DELETE -H "Content-Type: application/json" -H "Authorization: Bearer $DO_TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$id" 
+  echo "Deleted TXT record with id=$id on Digital Ocean"
+done
+
+# Create new record with $NAME and $VALUE
+RESP_CREATE=$(curl -sS -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $DO_TOKEN" -d '{"type":"TXT","name":"'"$NAME"'","data":"'"$VALUE"'","priority":null,"port":null,"ttl":1800,"weight":null,"flags":null,"tag":null}' "https://api.digitalocean.com/v2/domains/$DOMAIN/records")
+echo "Created TXT record $NAME.$DOMAIN -> $VALUE on Digital Ocean"
