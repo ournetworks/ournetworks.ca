89236fa887e6fc0f39b83fff41c44b66355f9f46
Benedict Lau <b@deprecated.systems>
2019-09-18 05:23:37 -0400

    Update video player

--- js/video-player-embed.js
+++ js/video-player-embed.js
@@ -1,21 +1,84 @@
-var live = videojs('live');
-var rootURL = window.location.href.split('?')[0]
-
-// For any browser except Safari
-if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent) === false) {
-  // Override native player for platform and browser consistency
-  videojs.options.html5.nativeAudioTracks = false;
-  videojs.options.html5.nativeVideoTracks = false;
-  videojs.options.hls.overrideNative = true;
+// Process URL params
+function getURLParam(key) {
+  return new URLSearchParams(window.location.search).get(key);
 }
+
+var ipfs_gw = getURLParam('gw');                          // Set custom IPFS gateway
+if (getURLParam('m3u8'))
+  var m3u8_ipfs = getURLParam('m3u8');                    // Set m3u8 file URL to override IPFS live stream
+var vod_ipfs = getURLParam('vod') || getURLParam('ipfs'); // Set IPFS content hash of mp4 file to play IPFS on-demand video stream ('ipfs' for backward compatability)
+var start_from = getURLParam("from");                     // Timecode to start video playing from
+
+// Configure default playback behaviour
+var stream_type = 'application/x-mpegURL'; // Type of video stream
+var stream_url_ipfs = m3u8_ipfs;           // Source of IPFS video stream
+var stream_urls_http = m3u8_http_urls;     // Source of HTTP video stream
+
+if (ipfs_gw) {
+  ipfs_gateway = ipfs_gw;
+}
+
+if (vod_ipfs) {
+  stream_type = 'video/mp4';
+  stream_url_ipfs = ipfs_gateway + '/ipfs/' + vod_ipfs;
+  stream_urls_http = [];
+  document.getElementById('selectingTitle').innerHTML = 'Select recorded stream source';
+}
+
+// If start_from is not a number it's probably an IPFS hash so calculate to correct start_from
+var hash="";
+if (start_from && +start_from != start_from) {
+  hash = start_from;
+  // Remove start_from value since the hash may not be in the list
+  start_from = undefined;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      file = this.response;
+      fileline = file.split("\n");
+      counter = 0;
+      // Loop through entries in the file
+      for (var a = 0; a < fileline.length; a++) {
+        // Look for EXTINF tags that describe the length of the chunk
+        if (fileline[a].indexOf("EXTINF:") > 0) {
+          // Parse out the length of the chunk
+          var number = fileline[a].substring(fileline[a].indexOf("EXTINF:") + 7);
+          number = number.substring(0, number.length - 1);
+          // Skip over chunk hash information
+          a++;
+          if (fileline[a].indexOf(hash) > 0) {
+            // If hash is found set the start_from to the counter and exit;
+            start_from = counter;
+            return;
+          }
+          // Add chunk length to counter
+          counter = counter + parseFloat(number);
+        }
+      }
+    }
+  };
+  xmlhttp.open("GET", m3u8_ipfs, true);
+  xmlhttp.send();
+}
+
+// Configure video player
+var live = videojs('live', { liveui: true });
+
+// Override native player for platform and browser consistency
+videojs.options.html5.nativeAudioTracks = false;
+videojs.options.html5.nativeVideoTracks = false;
+videojs.options.hls.overrideNative = true;
 
 function httpStream() {
   live.src({
-    src: stream_urls_http[Math.floor(Math.random() * stream_urls_http.length)],
+    src: stream_urls_http[Math.floor(Math.random() * m3u8_http_urls.length)],
     type: stream_type
   });
   loadStream();
 }
+
+// Counter to track video playback state
+var streamState = 0;
 
 function ipfsStream() {
   live.src({
@@ -24,11 +87,40 @@ function ipfsStream() {
   });
   loadStream();
   videojs.Hls.xhr.beforeRequest = function(options) {
-    // Replace IPFS gateway of origin with that of this node
-    options.uri = options.uri.replace(ipfs_gateway_origin, ipfs_gateway_self);
-    if (options.uri.indexOf('/ipfs/')) {
+
+    // When .m3u8 is loaded, start playback and transition to streamState = 1
+    if (options.uri.indexOf('.m3u8') > 0) {
+      if (!streamState) {
+        live.play();
+        streamState = 1;
+      }
+    }
+
+    if (options.uri.indexOf('/ipfs/') > 0) {
       document.getElementById('loadingTitle').innerHTML = 'Located stream via IPFS';
       document.getElementById('msg').innerHTML = 'Downloading video content...';
+      // Use specified IPFS gateway by replacing it in the uri
+      options.uri = ipfs_gateway + options.uri.substring(options.uri.indexOf('/ipfs/'));
+
+      // Wait for two .ts chunks to be loaded before applying seek action
+      if (streamState < 3) {
+        streamState++;
+        if (streamState == 3) {
+          if (!start_from) {
+            // Seek to live after waiting 1 s
+            setTimeout(function() { live.liveTracker.seekToLiveEdge(); }, 1);
+          } else {
+            // Seek to start_from time after waiting 1 s
+            setTimeout(function() { live.currentTime(start_from); }, 1);
+          }
+        }
+      }
+    }
+
+    if (options.uri.indexOf('/ipns/') > 0) {
+      document.getElementById('loadingTitle').innerHTML = 'Located stream via IPFS';
+      document.getElementById('msg').innerHTML = 'Downloading video content...';
+      options.uri = ipfs_gateway + options.uri.substring(options.uri.indexOf('/ipns/'));
     }
     console.debug(options.uri);
     return options;
@@ -36,8 +128,8 @@ function ipfsStream() {
 }
 
 function loadStream() {
-  document.getElementById('LoadingStream').style.display = 'block';
-  document.getElementById('SelectStream').style.display = 'none';
+  document.getElementById('loadingStream').style.display = 'block';
+  document.getElementById('selectStream').style.display = 'none';
 }
 
 document.querySelector('.ipfs-stream').addEventListener('click', function(event) {
@@ -51,7 +143,7 @@ document.querySelector('.http-stream').addEventListener('click', function(event)
 live.metadata = 'none';
 
 live.on('loadedmetadata', function() {
-  document.getElementById('StreamSelector').style.display = 'none';
+  document.getElementById('streamSelector').style.display = 'none';
 });
 
 live.on('loadeddata', function(event) {
@@ -67,14 +159,10 @@ refreshButton.addEventListener('click', function() {
 
 live.on('error', function(event) {
   console.debug(this.error());
-  if (window.location.href.indexOf('livestream') > -1) {
-    document.getElementById('loadingTitle').innerHTML = 'Unable to load live stream';
-  } else {
-    document.getElementById('loadingTitle').innerHTML = 'Unable to load video';
-  }
+  document.getElementById('loadingTitle').innerHTML = 'Unable to load video stream';
   document.querySelector('.loader-animation').style.display = 'none';
   document.getElementById('msg').innerHTML = this.error().message;
-  document.getElementById('LoadingStream').appendChild(refreshButton);
+  document.getElementById('loadingStream').appendChild(refreshButton);
 });
 
 if (!stream_urls_http || !Array.isArray(stream_urls_http) || (stream_urls_http.length === 0)) {
@@ -82,17 +170,31 @@ if (!stream_urls_http || !Array.isArray(stream_urls_http) || (stream_urls_http.l
 }
 
 // Video sharing links
+var rootURL = window.location.href.split('?')[0];
+
+function getShareLink(key) {
+  var bookmark = live.currentTime(); // TODO Change to use ipfs hash
+  return `${rootURL}?m3u8=${m3u8_ipfs}&from=${bookmark}`;
+}
+
+setInterval(function () {
+  var link = document.getElementById('link');
+  link.value = getShareLink();
+}, 1000);
 
 document.querySelector('.share-tweet').addEventListener('click', function() {
-  var tweetURL = `${rootURL}?from=${live.currentTime()}&m3u8=live.m3u8`
-  window.open(`https://twitter.com/intent/tweet?url=${encodeURIComponent(tweetURL)}`)
+  var link = document.getElementById('link');
+  link.value = getShareLink();
+  const tweetURL = link.value;
+  window.open(`https://twitter.com/intent/tweet?url=${encodeURIComponent(tweetURL)}`);
 });
 
 document.querySelector('.share-link').addEventListener('click', function() {
   var link = document.getElementById('link');
-  link.value= `${rootURL}?from=${live.currentTime()}&m3u8=live.m3u8`;
+  link.value = getShareLink();
   link.select();
   link.setSelectionRange(0, 99999); // For mobile devices
   document.execCommand('copy');
-  alert('In clipboard');
+  alert('Link copied to clipboard');
 });
+
