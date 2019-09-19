e302875a658a0c98f4313edf3809fd10b210dc50
Benedict Lau <b@deprecated.systems>
2019-09-18 23:58:06 -0400

    Update video player (#142)
    * Update video player
    
    * Update share UI
    
    * Update _video_js.scss with videojs css
    
    * Fix window carriage returns (#143)
    
    * flip order
    
    * add frontmatter for non-sticky nav
    
    * Add hash-based bookmark (#144)

--- js/live-stream-embed.js
+++ js/live-stream-embed.js
@@ -1,8 +1,4 @@
 // Live Stream Video
-
-var ipfs_gateway_self = 'https://ipfs-gateway.live.mesh.world'; // IPFS gateway of this node
-var ipfs_gateway_origin = 'https://ipfs-gateway.mesh.world'; // IPFS gateway of origin stream
-
-var stream_type = 'application/x-mpegURL'; // Type of video stream
-var stream_url_ipfs = 'https://live.mesh.world/live.m3u8'; // File path to stream with IPFS content via HTTP server
-var stream_urls_http = ['https://stream.aseriesoftubez.com/hls/tomesh/ournetworks.m3u8']; // Optional list of URLs to stream over HTTP
+var ipfs_gateway = 'https://ipfs-gateway.live.mesh.world'; // IPFS gateway
+var m3u8_ipfs = 'https://live.mesh.world/live.m3u8'; // HTTP or local path to m3u8 file containing IPFS content
+var m3u8_http_urls = ['https://sc1.asotcdn.xyz/hls/ournetworks/live.m3u8']; // HTTP or local paths to m3u8 file containing HTTP content (optional)
--- js/vod-stream-embed.js
+++ js/vod-stream-embed.js
@@ -1,23 +1,4 @@
 // Video-on-demand Stream Video
-
-var ipfs_gateway_self = 'https://ipfs.infura.io'; // IPFS gateway of this node
-var ipfs_gateway_origin = 'https://ipfs.io'; // IPFS gateway of origin stream
-
-var stream_type = 'video/mp4'; // Type of video stream
-var stream_url_ipfs = 'https://ipfs.io/ipfs/'; // File path to stream with IPFS content via HTTP server
-var stream_urls_http = []; // Optional list of URLs to stream over HTTP
-
-// Replace IPFS gateway of origin with that of this node
-stream_url_ipfs = stream_url_ipfs.replace(ipfs_gateway_origin, ipfs_gateway_self);
-
-// Append IPFS content to stream URL
-var ipfs_param = new URLSearchParams(window.location.search).get('ipfs');
-if (ipfs_param) {
-  stream_url_ipfs = stream_url_ipfs + ipfs_param;
-}
-
-// Add URL to stream over HTTP if available
-var http_param = new URLSearchParams(window.location.search).get('http');
-if (http_param) {
-  stream_urls_http.push(http_param);
-}
+var ipfs_gateway = 'https://ipfs.infura.io'; // IPFS gateway
+var m3u8_ipfs = ''; // HTTP or local path to m3u8 file containing IPFS content
+var m3u8_http_urls = []; // HTTP or local paths to m3u8 file containing HTTP content (optional)
