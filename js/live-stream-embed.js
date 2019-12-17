b14f7ca81ff13fa663e8073174ff12a7128ec8ba
Benedict Lau <b@deprecated.systems>
2019-12-17 11:13:26 +0200

    Switch to ipfs.io gateway for live and vod streams

--- js/live-stream-embed.js
+++ js/live-stream-embed.js
@@ -1,4 +1,4 @@
 // Live Stream Video
-var ipfs_gateway = 'https://ipfs-gateway.live.mesh.world'; // IPFS gateway
+var ipfs_gateway = 'https://ipfs.io'; // IPFS gateway
 var m3u8_ipfs = 'https://live.mesh.world/live.m3u8'; // HTTP or local path to m3u8 file containing IPFS content
 var m3u8_http_urls = []; // HTTP or local paths to m3u8 file containing HTTP content (optional)
