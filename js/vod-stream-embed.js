cabf90923718d7f74daef2fad70685a6db1b3011
Garry Ing <garry.ing@gmail.com>
2020-03-21 18:59:40 -0400

    initial reset

--- js/vod-stream-embed.js
+++
@@ -1,6 +0,0 @@
----
----
-// Video-on-demand Stream Video
-var ipfs_gateway = '{{ site.ipfs_gateway }}'; // IPFS gateway
-var m3u8_ipfs = ''; // HTTP or local path to m3u8 file containing IPFS content
-var m3u8_http_urls = []; // HTTP or local paths to m3u8 file containing HTTP content (optional)
