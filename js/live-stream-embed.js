a60ac682177f94a37e94b99aa6c591cabebba089
Garry Ing <garry.ing@gmail.com>
2020-09-06 15:45:48 -0400

    Adding live stream page (#205)
    * adding live stream
    
    * formatting

---
+++ js/live-stream-embed.js
@@ -0,0 +1,23 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+  var player = new Clappr.Player({
+      autoPlay: true,
+      height: '100%',
+      width: '100%',
+      source: 'https://vc1.asotcdn.xyz/hls/ournetworks/live.m3u8',
+      parentId: '.player',
+      plugins: [LevelSelector],
+      levelSelectorConfig: {
+        title: 'Quality',
+        labels: {
+          2: '1080p',
+          1: '720p',
+          0: '480p',
+        },
+        onLevelsAvailable: function(levels) {
+          return levels.reverse()
+        }
+      }
+    }
+  )
+})
